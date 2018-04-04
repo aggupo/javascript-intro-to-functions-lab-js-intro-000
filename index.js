89cbb6877cbc8d37c2b21ceea6e7187f5993dad6
Li Ge <gelber.libi@gmail.com>
2018-04-04 09:52:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   switch (string) {
+    case string == "I love you, Grandma.":
+      return "I love you, too.";
+      break;
     case string == string.toLowerCase():
       return "I can\'t hear you!";
       break;
     case string == string.toUpperCase():
       return "YES INDEED!";
-      break;
-    case string == "I love you, Grandma.":
-      return "I love you, too.";
       break;
   }
 }
